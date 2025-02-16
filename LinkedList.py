44b10ad19a1e1def7d81a720167644a3c47c55b2
K A R M A ☯️ <torangisb@gmail.com>
2025-02-16 12:48:51 +0000

    updated

--- LinkedList.py
+++ LinkedList.py
@@ -1,1 +1,43 @@
-print("Hello")
+class Node:
+    def __init__(self,value):
+        self.value = value 
+        self.next = None
+
+class LinkedList : 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length+=1
+    
+    def disp(self):
+        temp_node = self.head
+        while temp_node:
+            print(temp_node.value)
+            temp_node = temp_node.next
+
+
+ll = LinkedList()
+
+ll.append(10)
+ll.append(20)
+ll.append(30)
+ll.append(40)
+
+print(ll.length)
+print(ll.head.value)
+print(ll.tail.value)
+
+
+print("\n ----- \n")
+ll.disp()
+print("\n ----- \n")
