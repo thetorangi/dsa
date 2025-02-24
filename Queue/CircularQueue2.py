c16b6a0af1ce3076872335805e96b1178016229f
K A R M A ☯️ <torangisb@gmail.com>
2025-02-24 05:15:01 +0000

    Updated

--- Queue/CircularQueue2.py
+++ Queue/CircularQueue2.py
@@ -7,16 +7,14 @@ class Queue :
         self.start = -1
         self.top = -1
     
-    def isExmpty(self):
+    def isEmpty(self):
         if self.top == -1 :
             return True
         else:
             return False
     
     def isFull(self):
-        if self.top + 1 == self.start :
-            return True
-        elif self.start == 0 and self.top +1 == self.maxSize:
+        if self.start == (self.top+ 1) % self.maxSize :
             return True
         else:
             return False
@@ -25,17 +23,14 @@ class Queue :
         if self.isFull():
             return "Queue is Full"
         else:
-            if self.top + 1 == self.maxSize:
-                self.top = 0
-            else:
-                self.top+=1
-                if self.start == -1 :
-                   self.start = 0
+            self.top = (self.top + 1) % self.maxSize
+            if self.start == -1 :
+                self.start = 0
             self.items[self.top] = value
             return f"Value {value} is added"
         
     def dequeue(self):
-        if self.isExmpty():
+        if self.isEmpty():
             return "Queue is Empty "
         else:
             val = self.items[self.start]
@@ -43,14 +38,12 @@ class Queue :
             if self.start == self.top :
                 self.start = -1
                 self.top = -1
-            elif self.start + 1 == self.maxSize:
-                self.start = 0
             else:
-                self.start+=1
+                self.start= (self.start + 1 ) % self.maxSize
             return f"Value {val} is removed"
             
     def peek(self):
-        if self.isExmpty():
+        if self.isEmpty():
             return "Empty Queue"
         else:
             return f"Peek element is {self.items[self.start]}"
@@ -58,6 +51,7 @@ class Queue :
     def disp (self):
         print(self.items)
     
+
 queue = Queue(5)
 
 queue.enqueue(10)
